4886bcd3ad5585e72f8ff133d41f364900f621d1
Egor Semenov <egor_semenov@epam.com>
2025-05-14 22:21:08 +0400

    init: initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import path from 'path'
+
+module.exports = {
+  mode: 'production',
+  entry: './index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
