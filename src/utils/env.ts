07220b9841e034b580e9f387f7474082fb3ac1f6
Egor Semenov <egor_semenov@epam.com>
2025-05-19 01:58:04 +0400

    fix: fix dotenv issue

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,8 @@
+import { config } from "dotenv"
+config()
+
 export const ENV = {
   HTTP_SERVER_PORT: Number(process.env.HTTP_SERVER_PORT || 8181),
   WS_SERVER_PORT: Number(process.env.WS_SERVER_PORT || 3000),
 }
+
